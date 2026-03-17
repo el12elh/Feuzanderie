3f0273d963c76753e752e4ab89c76ee769e9cb00
El Mehdi El Haddad <elmehdi.elhaddad@icloud.com>
2026-03-17 11:51:21 +0100

    Set new Time Zone +01:00

--- db.php
+++ db.php
@@ -10,13 +10,14 @@ $pass = '';
 $charset = 'utf8mb4';
 
 try {
-     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-     $pdo = new PDO($dsn, $user, $pass, [
-         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-     ]);
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    $pdo->exec("SET time_zone = '+01:00'");
 } catch (PDOException $e) {
-     // This will print the error to the screen so you can debug
-     die("Connection failed: " . $e->getMessage());
+    // This will print the error to the screen so you can debug
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
