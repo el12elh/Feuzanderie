3e0a0b68c591248cbb754375fbbb5da24810a78d
El Mehdi El Haddad <elmehdi.elhaddad@icloud.com>
2026-03-17 11:52:30 +0100

    Refactor the export transactions

--- export_transactions.php
+++ export_transactions.php
@@ -2,42 +2,10 @@
 session_start();
 
 // Database connection
-require_once 'db.php';
-
-// Fetch data (Your existing SQL query)
-$tr_stmt = $pdo->prepare("
-    SELECT 
-        'TOPUP' AS TYPE,
-        CONCAT(c.FIRST_NAME, ' ', c.LAST_NAME) AS CUSTOMER,
-        r.NAME AS LABEL,
-        t.AMOUNT AS AMOUNT,
-        t.CREATED_AT,
-        CONCAT(a.FIRST_NAME, ' ', a.LAST_NAME) AS BY_NAME
-    FROM wallet_topup t
-    JOIN customers c ON t.ID_CUSTOMER = c.ID_CUSTOMER
-    LEFT JOIN users_customers uc ON t.ID_USER = uc.ID_USER
-    LEFT JOIN customers a ON uc.ID_CUSTOMER = a.ID_CUSTOMER
-    LEFT JOIN ref_topup_type r ON t.ID_TOPUP_TYPE = r.ID_TOPUP_TYPE
-    UNION ALL
-    SELECT 
-        'PURCHASE' AS TYPE,
-        CONCAT(c.FIRST_NAME, ' ', c.LAST_NAME) AS CUSTOMER,
-        CONCAT(tr.QUANTITY, 'x', p.NAME) AS LABEL,
-        -(p.PRICE * tr.QUANTITY) AS AMOUNT,
-        tr.CREATED_AT,
-        CONCAT(a.FIRST_NAME, ' ', a.LAST_NAME) AS BY_NAME
-    FROM transactions tr
-    JOIN customers c ON tr.ID_CUSTOMER = c.ID_CUSTOMER
-    LEFT JOIN users_customers uc ON tr.ID_USER = uc.ID_USER
-    LEFT JOIN customers a ON uc.ID_CUSTOMER = a.ID_CUSTOMER
-    LEFT JOIN ref_product p ON tr.ID_PRODUCT = p.ID_PRODUCT
-    ORDER BY CREATED_AT DESC, CUSTOMER
-");
-$tr_stmt->execute();
-$tr = $tr_stmt->fetchAll(PDO::FETCH_ASSOC);
+include 'db.php';
+include 'queries.php';
 
 // --- CSV GENERATION START ---
-
 $filename = "transactions_" . date('YmdHis') . ".csv";
 
 // Clear any previous output to prevent file corruption
@@ -54,12 +22,12 @@ $output = fopen('php://output', 'w');
 fprintf($output, chr(0xEF).chr(0xBB).chr(0xBF));
 
 // 2. Write the Column Headers
-fputcsv($output, array('Type', 'Member', 'Details', 'Amount', 'Date', 'By'));
+fputcsv($output, array('Member', 'Details', 'Amount', 'Date', 'By'));
 
 // 3. Write the Data Rows
-foreach ($tr as $row) {
-    fputcsv($output, $row);
-}
+    foreach ($all_transactions as $tr) {
+        fputcsv($output, $tr);
+    }
 
 // Close the stream
 fclose($output);
